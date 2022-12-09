9c2411e7ea3e11fcc37b8848523f93f4a9d45f49
Mickey MacDonald <mimacdo@microsoft.com>
2022-12-09 16:31:24 -0500

    Delete {{cookiecutter.project_slug}}/notebooks directory

--- {{cookiecutter.project_slug}}/notebooks/{{cookiecutter.project_slug}}_orchestrator.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %md # Runner
-
-# COMMAND ----------
-
-from {{cookiecutter.project_slug}} import {{cookiecutter.project_slug}}
-
-{{cookiecutter.project_slug}}.example_("Example Called").display()
